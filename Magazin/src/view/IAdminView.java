1fa6829e5476398687d9a465fdb8e67228265285
SanyiMoriczi <56352289+moriczisanyi@users.noreply.github.com>
2021-03-25 17:41:38 +0200

    Add files via upload

---
+++ Magazin/src/view/IAdminView.java
@@ -0,0 +1,10 @@
+package view;
+
+public interface IAdminView {
+	abstract String numeText();
+	abstract String usernameText();
+	abstract String parolaText();
+	abstract String varstaText();
+	abstract String boxMagazinText();
+	abstract String boxAngajatText();
+}
